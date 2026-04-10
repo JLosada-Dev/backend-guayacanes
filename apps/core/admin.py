7054c434995741ec1b6192bca5bdb7ab38c324b8
JLosada-Dev <jd.losada.dev@gmail.com>
2026-04-10 10:03:52 -0500

    feat: configure core admin models and add initial service and aspect fixtures

---
+++ apps/core/admin.py
@@ -0,0 +1,34 @@
+from django.contrib.gis import admin
+from .models import Commune, Neighborhood, Service, Aspect
+
+
+@admin.register(Commune)
+class CommuneAdmin(admin.GISModelAdmin):
+    list_display  = ['number', 'name', 'area_hectares']
+    ordering      = ['number']
+    search_fields = ['name', 'number']
+
+
+@admin.register(Neighborhood)
+class NeighborhoodAdmin(admin.GISModelAdmin):
+    list_display  = ['name', 'commune', 'dane_code', 'osm_id']
+    ordering      = ['commune__number', 'name']
+    search_fields = ['name']
+    list_filter   = ['commune']
+    raw_id_fields = ['commune']
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display  = ['name', 'slug', 'active', 'order']
+    ordering      = ['order']
+    search_fields = ['name', 'slug']
+    list_filter   = ['active']
+
+
+@admin.register(Aspect)
+class AspectAdmin(admin.ModelAdmin):
+    list_display  = ['description', 'service', 'slug', 'active']
+    ordering      = ['service__order', 'description']
+    search_fields = ['description', 'slug']
+    list_filter   = ['service', 'active']
